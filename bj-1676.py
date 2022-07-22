e2e183ee48c0f0490a0068a583859e497be1546c
overflow218 <gagu1999@kaist.ac.kr>
2022-07-22 17:54:32 +0900

    220722 오늘의 알고리즘1

--- bj-1676.py
+++ bj-1676.py
@@ -1,15 +1,20 @@
-def count(n):
+import math
+def count(x):
     cnt = 0
-    while(n > 0 and n % 10 == 0):
+    while(x > 0 and x % 10 == 0):
         cnt += 1
-        n = n // 10 
+        x = x // 10 
     return cnt
 n = int(input())
-last, cnt = 1, 0 if n > 0 else 1
-for i in range(1, n + 1):
-    last = last * i
-    c = count(last)
-    cnt = cnt + c
-    x = str(last)[:-c]
-    last = int(str(last)[:-c][0]) if c > 0 else last % 10
-print(cnt)
+print(count(math.factorial(n)))
+
+n = int(input())
+cnt2, cnt5 = 0, 0
+for i in range(2, n + 1):
+    while(i % 2 == 0):
+        cnt2 += 1
+        i = i // 2
+    while(i % 5 == 0):
+        cnt5 += 1
+        i = i // 5
+print(min(cnt2, cnt5))
