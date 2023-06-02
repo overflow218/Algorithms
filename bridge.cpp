5be30a5d4d11a4f30b3b6147d97818861b4a7560
overflow218 <gagu1999@kaist.ac.kr>
2023-06-02 22:03:24 +0900

    feat: 230602 today algorithm

--- bridge.cpp
+++ bridge.cpp
@@ -1,13 +1,63 @@
 #include "bridge.h"
+using namespace std;
 
 void find_bridges(int N)
 {
+    vector<int> countL(N + 1, 0);
+    vector<int> countR(N + 1, 0);
+    vector<int> left;
+    vector<int> right;
+    int prevCount = 0;
     for(int i = 1; i <= N; i++)
     {
-        for(int j = 1; j <= N; j++)
+        int cnt = count_crosses(1, i);
+        
+        if(cnt == -1)
         {
-            if(count_crosses(i, j) == -1)
-                report_bridge(i, j);
+            countL[1]++;
+            countR[i]++;
+            prevCount = 0;
+            continue;
         }
+        countR[i - 1] += cnt - prevCount;
+        prevCount = cnt;
+    }
+    prevCount = 0;
+    for(int i = N; i > 0; i--)
+    {
+        int cnt = count_crosses(i, N);
+        if(cnt == -1)
+        {
+            if(i == 1) continue;
+            countL[i]++;
+            countR[N]++;
+            prevCount = 0;
+            continue;
+        }
+        countL[i + 1] += cnt - prevCount;
+        prevCount = cnt;
+    }
+
+    for(int i = 1; i <= N; i++)
+    {
+        if(countL[i] > 0)
+        {
+            for(int j = 0; j < countL[i]; j++)
+            {
+                left.push_back(i);
+            }
+        }
+        if(countR[i] > 0)
+        {
+            for(int j = 0; j < countR[i]; j++)
+            {
+                right.push_back(i);
+            }
+        }
+    }
+
+    for(int i = 0; i < left.size(); i++)
+    {
+        report_bridge(left[i], right[i]);
     }
 }
--- jelly.cpp
+++ jelly.cpp
@@ -1,30 +1,75 @@
 #include "jelly.h"
 using namespace std;
 
+vector<int> ans;
+int find(vector<int> vec)
+{
+    if(vec.size() == 1) return vec[0];
+    int mid = vec.size() / 2;
+
+    vector<int> left(vec.begin(), vec.begin() + mid);
+    vector<int> right(vec.begin() + mid, vec.end());
+    vector<int> tmp(ans.begin(), ans.end());
+
+    for(int i = 0; i < left.size(); i++)
+    {
+        tmp.push_back(left[i]);
+    }
+
+    // 왼쪽에 들어있는 상황임.
+    if(query(tmp) == query(left)) return find(left);
+    
+    // 오른쪽에 들어있는 상황임.
+    return find(right);
+}
+
 std::vector<int> start(int N)
 {
-    std::vector<int> ans;
-    vector<bool> isDone(N + 1, false);
-    ans.push_back(1);
-    isDone[1] = true;
-    
+    vector<int> leftJelly;
+    if(N == 1) return {1};
 
-    while(ans.size() < N)
+    for(int i = 1; i <= N; i++)
     {
-        for(int i = 1; i <= N; i++)
+        leftJelly.push_back(i);
+    }
+
+    for(int i = 0; i < N; i++)
+    {
+        vector<int> tmp(leftJelly.begin(), leftJelly.end());
+        tmp.erase(tmp.begin() + i);
+        // i + 1을 시작으로 삼아도 좋음. 
+        if(query(tmp) == 1)
         {
-            if(isDone[i]) continue;
-            vector<int> vec(ans.begin(), ans.end());
-            vec.push_back(i);
-            int result = query(vec);
-            if(result == 1)
-            {
-                isDone[i] = true;
-                ans.push_back(i);
-            }
+            ans.push_back(i + 1);
+            leftJelly.erase(leftJelly.begin() + i);
+            break;
         }
     }
     
+    for(int t = 0; t < N - 1; t++)
+    {
+        int next = find(leftJelly);
+        for(int i = 0; i < leftJelly.size(); i++)
+        {
+            if(leftJelly[i] == next)
+            {
+                leftJelly.erase(leftJelly.begin() + i);
+                break;
+            }
+        }
+        ans.push_back(next);
+    }
 
     return ans;
 }
+
+/*
+5
+4 2 3 1 5
+
+8
+1 7 2 3 6 5 4 8
+
+8
+1 7 8 2 3 6 5 4
+*/
